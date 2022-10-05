fd6654bfef247d64dde21da2a31a6af1d71490f6
Nikita K <w.feelingalright.p@yandex.ru>
2022-10-05 20:23:06 +0300

    feat: add search bar component

---
+++ react-components/src/components/search-bar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input className="search-bar" type="search" placeholder="search..."></input>;
+  }
+}
+
+export default SearchBar;
--- react-components/src/pages/main.tsx
+++ react-components/src/pages/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import SearchBar from 'components/search-bar';
+import SearchBar from 'components/search-bar';
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        {/* <SearchBar /> */}
+        <SearchBar />
         <p>~~~~~</p>
         <p>~~~~~</p>
         <h2>Main page content</h2>
