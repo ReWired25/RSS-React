640f22b2b497cda2fc91bdd83fa50af8abfc4f93
Nikita K <w.feelingalright.p@yandex.ru>
2022-10-06 13:11:49 +0300

    feat: add cards styles

--- react-components/src/types/enums.ts
+++ react-components/src/types/enums.ts
@@ -1,3 +1,9 @@
 export enum Storage {
   key = 'searchInputValue',
 }
+
+export enum CardStyles {
+  textShadow = '1px 0 #000, -1px 0 #000, 0 1px #000, 0 -1px #000, 1px 1px #000, -1px -1px #000, 1px -1px #000, -1px 1px #000',
+  differentColors = 'different',
+  defaultColor = 'black',
+}
